2ac846b787456fab01b6284da567018b3728b8ba
RGD2 <remydyer@gmail.com>
2023-02-02 16:07:42 +1100

    This version works - for some value of "works" that includes a wonky sinewave

--- fakeengine.py
+++ fakeengine.py
@@ -6,7 +6,7 @@ speed = 400 # RPM
 st = 120/400 / 1440
 c = int( st / (1/40e6))
 import numpy as np
-sine = np.clip(32767*np.sin( np.linspace(0, 2*np.pi, 1440)-0.5*np.pi )+32767, 0, 65535)
+sine = np.clip(32767*(0.8*np.sin( np.linspace(0, 2*np.pi, 1440)-0.5*np.pi)+0.2 )+32767, 0, 65535)
 i = 0
 with serial.Serial('/dev/ttyAMA0', 2500000) as ser:
     while (True):
@@ -15,5 +15,4 @@ with serial.Serial('/dev/ttyAMA0', 2500000) as ser:
             i += 1
             if i==1440:
                 i=0
-            sleep(st)
 
