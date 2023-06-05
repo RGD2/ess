9374c134b7571090dde7b640d9e8d2b32391d0b8
RGD2 <remydyer@gmail.com>
2023-06-05 16:18:44 +1000

    pi on edge now on isolated class c

--- dicepi.sh
+++ dicepi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make && scp ess.bin dice@dicepi1bp-nc.det.csiro.au:~/ && ssh dice@dicepi1bp-nc.det.csiro.au icezerotools/icezprog ess.bin
+make && scp ess.bin dice@dicepi:~/ && ssh dice@dicepi icezerotools/icezprog ess.bin
