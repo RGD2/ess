ce187cfcfb34419a18c3f851143dcaa6da425026
RGD2 <remydyer@gmail.com>
2023-06-05 16:03:46 +1000

    Slight rewrite of python fake engine code: only really does 130 RPM
    the C code manages about 300 RPM. Both are fairly cruddy on a Pi1B+,
    but it is good enough for synthetic signals to test the actual engine control system.

--- fakeengine.py
+++ fakeengine.py
@@ -2,11 +2,12 @@
 import serial
 from struct import pack
 from time import sleep, perf_counter_ns
-speed = 100 # RPM
-st = 120/400 / 1440
+speed = 400 # RPM
+st = 120/speed/1440
 c = int( st / (1/40e6))
 import numpy as np
 sine = np.clip(32767*(0.7*np.sin( np.linspace(0, 2*np.pi, 1440)-0.5*np.pi)+0.21 )+32767, 0, 65535)
+sint = [int(s) for s in sine]
 i = 0
 dt = 0
 lt = perf_counter_ns()
@@ -17,15 +18,16 @@ wait =0
 try:    
     with serial.Serial('/dev/ttyAMA0', 2500000) as ser:
         while (True):
-                now = perf_counter_ns()
-                dt = now-lt
-                lt = now
-
-                v = sine[i]
-                ser.write(pack('>HH', c, int(v)))
+                v = sint[i]
+                ser.write(pack('>HH', c, v))
+                ser.flush() # wait until done
                 i += 1
                 if i==1440:
                     i=0
+                    
+                now = perf_counter_ns()
+                dt = now-lt
+                lt = now
                 wait = st - dt
                 if wait>0:
                     sleep(wait)
